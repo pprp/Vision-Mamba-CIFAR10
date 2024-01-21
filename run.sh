c23a1354608b1793a990a1e41e0795cb9a9b2f83
pprp <1115957667@qq.com>
2024-01-21 13:12:38 +0800

    Refactor MambaBlock class and update run.sh parameters

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 CUDA_VISIBLE_DEVICES=6 python main.py --data-set CIFAR \
     --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_rope_also_residual \
     --data-path ./data \
-    --epochs 200 --lr 5e-3 \
-    --batch-size 640 --gpu 6
+    --epochs 200 --lr 5e-4 \
+    --batch-size 64 --gpu 6
